91b4b7f5bfa57b1ea705b168c0aec028a7bad93d
jensen yuen <jensenyuen@outlook.com>
2022-01-25 05:55:54 +0800

    Added code into spec files

--- spec/game_controller_spec.rb
+++ spec/game_controller_spec.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
 require_relative '../lib/controller/game_controller'
+require_relative '../lib/model/player'
 require_relative '../lib/view/game_view'
 
-RSpec.describe Gamecontroller, '#initialize' do
-  it 'initialize to create an instance of view' do
-    @view = GameView.new
-    expect(@view.class).to eq GameView
+RSpec.describe GameController do
+  let(:controller) { GameController.new }
+
+  describe '#initialize' do
+    it 'Initialize to create an instance of view' do
+      expect(controller
+        .instance_variable_get(:@view))
+        .to be_a GameView
+    end
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,1 +1,44 @@
 # frozen_string_literal: true
+
+require_relative '../lib/model/player'
+
+RSpec.describe Player do
+  let(:player) { Player.new('player') }
+  let(:VALID_HANDS) { %w[rock paper scissors] }
+
+  describe '#initialize' do
+    it 'Should set current choice to be empty' do
+      expect(player
+        .instance_variable_get(:@current_choice))
+        .to be_empty
+    end
+    it 'Should set name of player' do
+      expect(player
+        .instance_variable_get(:@name))
+        .to be_a String
+    end
+  end
+  describe '#assign_hand' do
+    it 'Should assign current choice as rock' do
+      player.current_choice = 1
+      player.assign_hand
+      expect(player.current_choice).to eq 'rock'
+    end
+    it 'Should assign current choice as paper' do
+      player.current_choice = 2
+      player.assign_hand
+      expect(player.current_choice).to eq 'paper'
+    end
+    it 'Should assign current choice as scissors' do
+      player.current_choice = 3
+      player.assign_hand
+      expect(player.current_choice).to eq 'scissors'
+    end
+  end
+  describe '#cpu' do
+    it 'Should assign a random hand to the cpu' do
+      player.cpu
+      expect(player.current_choice).to be_a String
+    end
+  end
+end
