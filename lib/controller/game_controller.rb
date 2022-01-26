64ce3fdffdadbe3e40084a663a7a543cf32a1c33
jensen yuen <jensenyuen@outlook.com>
2022-01-26 20:14:58 +0800

    Added extendable code for game logic

--- lib/controller/game_controller.rb
+++ lib/controller/game_controller.rb
@@ -75,6 +75,8 @@ class GameController
   end
 
   def valid_hand?(player_hand)
+    # Extendable code
+    # %w[] would include 4(spock) and 5(scissors)
     if %w[1 2 3].include?(player_hand)
       @player.current_choice = player_hand
       @player.assign_hand
@@ -98,6 +100,12 @@ class GameController
     else
       scissors_win?(right_player_hand) ? @view.scissors_win(left_player.name) : @view.scissors_lose(right_player.name)
     end
+    # =========== start of extendable code ===========
+    # when 'spock'
+    #   spock_win?(right_player_hand) ? @view.spock_win(left_player.namd) : @view.spock_lose(right_player.name)
+    # when 'lizard
+    #   lizard_win?(right_player_hand) ? @view.lizard_win(left_player.namd) : @view.lizard_lose(right_player.name)
+    # ============ End of extendable code ============
   end
 
   def tie?(left_player, right_player)
@@ -117,6 +125,12 @@ class GameController
     else
       @view.scissors_tie
     end
+    # =========== start of extendable code ===========
+    # when 'spock'
+    #   @view.spock_tie
+    # when 'lizard'
+    #   @view.lizard_tie
+    # ============ End of extendable code ============
   end
 
   # =========== start of methods to test for winning hand ===========
@@ -130,17 +144,42 @@ class GameController
     # return true if right player has scissors
     # return false if right player has paper
     right_player_hand == 'scissors'
+    # Extenable code
+    # condition will be re-write as %w[scissors lizard].include?(right_player_hand)
   end
 
   def paper_win?(right_player_hand)
     # return true if right player has rock
     # return false if right player has scissors
     right_player_hand == 'rock'
+    # Extenable code
+    # condition will be re-write as %w[rock lizard].include?(right_player_hand)
   end
 
   def scissors_win?(right_player_hand)
     # return true if right player has paper
     # return false if right player has rock
     right_player_hand == 'paper'
+    # Extenable code
+    # condition will be re-write as %w[paper spock].include?(right_player_hand)
   end
+
+  # =========== start of extendable code ===========
+  # assuming Rock paper scissors spock lizard
+  # win conditions for player
+  # rock beats scissors and lizard
+  # paper beats rock and spock
+  # scissors beats paper and spock
+  # spock beats scissors and rock
+  # lizard beats spock and paper
+  # lose for any other combination
+
+  # def spock_win?(right_player_hand)
+  #   %w[scissors rock].include?(right_player_hand)
+  # end
+
+  # def lizard_win?(right_player_hand)
+  #   %w[spock paper].include?(right_player_hand)
+  # end
+  # ============ End of extendable code ============
 end
